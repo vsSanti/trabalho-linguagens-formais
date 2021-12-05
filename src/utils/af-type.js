82e771dd1699f6470dc1d654b7ad8076bc905c99
Vinícius Santiago <viniciussdsilva@gmail.com>
2021-12-05 13:28:02 -0300

    refactor: enhance getAFType code

--- src/utils/af-type.js
+++ src/utils/af-type.js
@@ -1,17 +1,17 @@
 const getAFType = (table, states) => {
+  // verifica se tem algum array de voids em um estado
   const hasVoidMovement = states.some((state) => table[state].void?.length);
+  // se tiver algum array de void, é um AFNDe
+  if (hasVoidMovement) return 'AFNDe';
+
+  // verifica se todas as transições possuem apenas 1 ou nenhum movimento.
   const isDeterministic = states.every((state) => {
     const transitionKeys = Object.keys(table[state]);
-    if (table[state].void?.length > 0) return false;
     return transitionKeys.every((transition) => table[state][transition].length <= 1);
   });
 
-  let afType = 'AFNDe';
-  if (!hasVoidMovement) {
-    afType = isDeterministic ? 'AFD' : 'AFND';
-  }
-
-  return afType;
+  // se for determinístico, é AFD. Caso contrário, é um AFND
+  return isDeterministic ? 'AFD' : 'AFND';
 }
 
 module.exports = { getAFType };
