ac16de77fd334c9437abc77d5b1b5064c2cf2d84
Vinícius Santiago <viniciussdsilva@gmail.com>
2021-12-05 13:04:20 -0300

    feat: convert AFND into AFD

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const entry = require('./src/data/AFND.json');
 const { getAFType } = require('./src/utils/af-type');
+const { convertAFNDintoAFD } = require('./src/utils/convert-afnd-into-afd');
 const { getECLOSURE } = require('./src/utils/e-closure');
 const { removeVoidTransition } = require('./src/utils/remove-void-transition');
 
 const { table: originalTable } = entry;
-let tableCopy = JSON.parse(JSON.stringify(originalTable));
+let af = JSON.parse(JSON.stringify(originalTable));
 const states = Object.keys(originalTable);
 
+// faz a verificação inicial do tipo do AF
 let afType = getAFType(originalTable, states);
 console.log(`O AF é um: ${afType}`);
 console.table(originalTable);
@@ -16,10 +18,18 @@ if (afType === 'AFNDe') {
   const eCLOSURE = getECLOSURE(originalTable, states);
 
   // remoção das transições vazias
-  tableCopy = removeVoidTransition(originalTable, states, eCLOSURE);
+  af = removeVoidTransition(originalTable, states, eCLOSURE);
   console.log('\nTabela sem os movimentos vazios:');
-  console.table(tableCopy);
+  console.table(af);
 
-  afType = getAFType(tableCopy, Object.keys(tableCopy));
+  // verifica novamente o tipo do AF após a remoção das transições vazias
+  afType = getAFType(af, Object.keys(af));
   console.log(`\nTipo do AF após remoção das transições vazias: ${afType}`);
+}
+
+if (afType === 'AFND') {
+  // faz a equivalência de AFND para AFD
+  af = convertAFNDintoAFD(af, states[0]);
+  console.log('\nTabela de equivalência entre o AFND e AFD:');
+  console.table(af);
 }
---
+++ src/utils/convert-afnd-into-afd.js
@@ -0,0 +1,51 @@
+const convertAFNDintoAFD = (table, initialState) => {
+  const afd = {};
+  // lista de estados que ainda precisam ser convertidos
+  // é uma lista do tipo FIFO
+  const queue = [[initialState]];
+  // chaves desse estado
+  const transitionKeys = Object.keys(initialState);
+
+  // enquanto tiver chave, fazer a procura na tabela
+  while (queue.length) {
+    // remove o primeiro estado da chave
+    const state = queue.shift();
+
+    // percorre todos os valores de transição
+    const newVal = transitionKeys.reduce((keyAcc, key) => {
+      // percorre todos os estados que precisam ser verificados
+      const resp = state.reduce((stateAcc, s) => {
+        // faz a junção entre o estado da redução e o que contém na tabela original para
+        // o estado e chave
+        const newTransition = [...new Set([...stateAcc, ...table[s][key]].sort())];
+
+        // caso seja um estado que ainda não foi posto na lista, adiciona ele no final
+        if (!afd[newTransition]) {
+          afd[newTransition] = {};
+          queue.push(newTransition);
+        }
+        return newTransition;
+      }, []);
+
+      keyAcc[key] = resp;
+
+      return keyAcc;
+    }, {});
+
+    afd[state] = newVal;
+  }
+
+  // Transformando os arrays nas chaves e transições em uma string
+  const response = {};
+  Object.keys(afd).forEach((keys) => {
+    const key = String(keys).replace(/,/g, '');
+    response[key] = {};
+    transitionKeys.forEach((tk) => {
+      response[key][tk] = afd[keys][tk].join('')
+    });
+  });
+
+  return response;
+}
+
+module.exports = { convertAFNDintoAFD };
